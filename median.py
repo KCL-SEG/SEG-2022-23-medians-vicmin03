f476ca8b559ce50bef4ab312be1988b4899c1d7d
vicmin03 <vicmin03@outlook.com>
2022-10-01 14:48:51 +0100

    Calculate the median of numbers list

--- median.py
+++ median.py
@@ -5,8 +5,17 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+        if (len(numbers)&2 == 0):
+            no1 = numbers[(len(numbers)/2)-1]
+            no2 = numbers[len(numbers)/2]
+            print((no1+no2)/2)
+        else:
+            print(numbers[len(numbers)])
+
     except ValueError:
-        print("Some input could not be converted to a number!")
+        checkIndex = int(len(numbers)/2)
+        print(numbers[checkIndex])
     else:
         break
-print(numbers)
+
